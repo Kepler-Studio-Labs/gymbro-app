51a93b9a24b25401a18b8af0a5394223dcdd6eec
codebyalexx <a.legal.contact@gmail.com>
2025-06-01 14:14:21 +0200

    :label: Added muscle groups constants

---
+++ src/lib/gym-data.ts
@@ -0,0 +1,23 @@
+export const upperMuscleGroups = [
+  "Poitrine",
+  "Dos",
+  "Épaules",
+  "Bras",
+  "Abdominaux",
+  "Avant-bras",
+  "Trapèzes",
+  "Cervicaux",
+  "Autre (Upper)",
+];
+
+export const lowerMuscleGroups = [
+  "Jambes",
+  "Fessiers",
+  "Mollets",
+  "Adducteurs",
+  "Ischio-jambiers",
+  "Quadriceps",
+  "Autre (Lower)",
+];
+
+export const muscleGroups = [...upperMuscleGroups, ...lowerMuscleGroups];
